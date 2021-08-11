a4ca107aee0c726c6f6c4bac2c35f0d67a679ffc
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-08-10 20:04:00 -0400

    finished part two

--- controllers/teams.js
+++ controllers/teams.js
@@ -12,4 +12,24 @@ const getByTeamId = (req, res) => {
   return res.send(specificTeam)
 }
 
-module.exports = { listOfTeams, getByTeamId }
+const createNewTeam = (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division
+  } = request.body
+
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('Missing required input')
+  }
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
+
+  teams.push(newTeam)
+
+  return response.status(201).redirect('/teams')
+
+  console.log(request.body)
+  console.log(id)
+}
+
+module.exports = { listOfTeams, getByTeamId, createNewTeam }
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
 const express = require('express')
 const app = express()
-const { listOfTeams, getByTeamId } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+const { listOfTeams, getByTeamId, createNewTeam } = require('./controllers/teams')
 
+// Adding top level parsing
+app.use(bodyParser.json())
 
 // Home Route
 app.get('/teams', listOfTeams)
@@ -10,8 +13,10 @@ app.get('/teams', listOfTeams)
 // Team specific Route
 app.get('/teams/:id', getByTeamId)
 
+// Create post route for creating a new team
+app.post('/', createNewTeam )
 
 // Setting up port
-app.listen(3000, () => {
-  console.log('Server listening on port 3000 :)')
+app.listen(3001, () => {
+  console.log('Server listening on port 3001 :)')
 })
