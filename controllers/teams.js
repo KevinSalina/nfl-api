30a01be0606c749a15022ad90ad6a282dedda91b
Kevin Salina <kevin.salina25@gmail.com>
2021-08-10 20:27:18 -0400

    part two

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,15 +1,25 @@
 const teams = require('../teams')
-
-
 const listOfTeams = (req, res) => {
   return res.send(teams)
 }
-
 const getByTeamId = (req, res) => {
   const { id } = req.params
   const specificTeam = teams.filter(team => team.id === parseInt(id))
-
   return res.send(specificTeam)
 }
+const createNewTeam = (request, response) => {
+  const { id, location, mascot, abbreviation, conference, division } = request.body
 
-module.exports = { listOfTeams, getByTeamId }
+  if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('Missing required input')
+  }
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
+
+  teams.push(newTeam)
+
+  return response.status(201).redirect('/teams')
+
+}
+module.exports = { listOfTeams, getByTeamId, createNewTeam }
