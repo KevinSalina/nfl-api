5380f1d3b64fdeb0288a53dbaa984b940b406fea
Kevin Salina <kevin.salina25@gmail.com>
2021-08-24 19:45:26 -0400

    All integretion tests work

--- controllers/teams.js
+++ controllers/teams.js
@@ -6,6 +6,7 @@ const listOfTeams = async (req, res) => {
 
   return res.send(teams)
 }
+
 const getByTeamId = async (req, res) => {
   const { id } = req.params
 
@@ -13,21 +14,22 @@ const getByTeamId = async (req, res) => {
 
   return res.send(foundTeam)
 }
+
 const createNewTeam = async (request, response) => {
   const {
     location, mascot, abbreviation, conference, division
   } = request.body
 
 
-  if (!location || !mascot || !abbreviation || !conference || !division) {
-    return response.status(400).send('Missing required input')
-  }
+  // if (!location || !mascot || !abbreviation || !conference || !division) {
+  //   return response.status(400).send('Missing required input')
+  // }
 
   const newTeam = await models.teams.create({
     location, mascot, abbreviation, conference, division
   })
 
-  return response.status(201).send(newTeam)
+  return response.send(newTeam)
 }
 
 module.exports = { listOfTeams, getByTeamId, createNewTeam }
---
+++ tests/controllers/teams.test.js
@@ -0,0 +1,55 @@
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const chai = require('chai')
+const { describe, it } = require('mocha')
+const { listOfTeams, getByTeamId, createNewTeam } = require('../../controllers/teams')
+const { allTeams, singleTeam } = require('../mocks/teams')
+const models = require('../../models')
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('teams controllers tests', () => {
+  describe('listOfTeams', () => {
+    it('retrieves and displays all teams from nfl db using res.send method', async () => {
+      // first argument
+      const stubFindAll = sinon.stub(models.teams, 'findAll').returns(allTeams)
+      const stubSend = sinon.stub()
+      const response = { send: stubSend }
+
+      await listOfTeams({}, response)
+
+      expect(stubSend).to.have.been.calledWith(allTeams)
+      expect(stubFindAll).to.have.callCount(1)
+    })
+  })
+
+  // Get by ID
+  describe('getByTeamId', () => {
+    it('retrieves team associated with id from db using res.send()', async () => {
+      const request = { params: { id: 1 } }
+      const stubSend = sinon.stub()
+      const response = { send: stubSend }
+      const stubFindOne = sinon.stub(models.teams, 'findOne').returns(singleTeam)
+
+      await getByTeamId(request, response)
+
+      expect(stubFindOne).to.have.been.calledWith({ where: { id: 1 } })
+      expect(stubSend).to.have.been.calledWith(singleTeam)
+    })
+  })
+
+  describe('createNewTeam', () => {
+    it('takes the req.body and creates a new team in db', async () => {
+      const request = { body: singleTeam }
+      const stubSend = sinon.stub()
+      const response = { send: stubSend }
+      const stubCreate = sinon.stub(models.teams, 'create').returns(singleTeam)
+
+      await createNewTeam(request, response)
+
+      expect(stubCreate).to.have.been.calledWith(singleTeam)
+      expect(stubSend).to.have.been.calledWith(singleTeam)
+    })
+  })
+})
